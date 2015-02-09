61f1a6963fe41c4643df0cecf38b8901fb396cd4
Dav Lizarraga <davgeek@outlook.com>
2015-02-08 17:29:33 -0700

    Bug Linux

--- index.php
+++ index.php
@@ -7,9 +7,9 @@
  * Licence: MIT.
  */
 
-require_once 'app\Request.php';
-require_once 'app\Response.php';
-require_once 'app\View.php';
+require_once 'app/Request.php';
+require_once 'app/Response.php';
+require_once 'app/View.php';
 
 // Obtener la variable url por GET.
 (empty($_GET['url'])) ? $url = null : $url = $_GET['url'];
